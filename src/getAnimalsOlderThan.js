5d90be360ea9cbf9a77db7dd69e7a86742d939fb
no_name <no@email.com>
2024-02-29 12:59:41 -0300

    Requisito 2 satisfeito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,18 @@ const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
   // seu código aqui
+  let all = true;
+  for (let indexSpecie = 0; indexSpecie < data.species.length; indexSpecie += 1) {
+    const specie = data.species[indexSpecie];
+    if (specie.name === animal) {
+      for (let indexAnimal = 0; indexAnimal < specie.residents.length; indexAnimal += 1) {
+        const animal = specie.residents[indexAnimal];
+        if (animal.age < age)
+          all = false;
+      }
+    }
+  }
+  return all;
 };
 
 module.exports = getAnimalsOlderThan;
