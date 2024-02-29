62747ad148c5c9cdc4ffd334016d5e71db17ea9f
no_name <no@email.com>
2024-02-29 00:26:56 -0300

    Requisito 1 satisfeito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
+const getSpeciesByIds = (...ids) => {
   // seu código aqui
+  const arraySpecies = [];
+  if (!ids) return arraySpecies;
+  for (let indexSpecies = 0; indexSpecies < data.species.length; indexSpecies += 1) {
+    const specie = data.species[indexSpecies];
+    if (ids.includes(specie.id))
+      arraySpecies.push(specie);
+  }
+  return arraySpecies;
 };
 
 module.exports = getSpeciesByIds;
