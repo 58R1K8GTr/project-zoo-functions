1c140a6003d451bbcb50856914a65e55dacf7765
b166erobot <bernard0freitas@outlook.com>
2024-03-06 23:30:30 -0300

    Requisito 1 arrumado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,12 +3,11 @@ const data = require('../data/zoo_data');
 const getSpeciesByIds = (...ids) => {
   // seu código aqui
   const arraySpecies = [];
-  if (!ids) return arraySpecies;
-  for (let indexSpecies = 0; indexSpecies < data.species.length; indexSpecies += 1) {
-    const specie = data.species[indexSpecies];
-    if (ids.includes(specie.id))
-      arraySpecies.push(specie);
-  }
+  data.species.forEach((item) => {
+    if (ids.includes(item.id)) {
+      arraySpecies.push(item)
+    }
+  });
   return arraySpecies;
 };
 
