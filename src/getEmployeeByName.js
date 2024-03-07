ae0a000c6b2f599cba00d1ccb29f7d4fa239d82a
no_name <no@email.com>
2024-03-06 23:48:21 -0300

    Requisito 3 satisfeito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
   // seu código aqui
+  const employee = data.employees
+    .find((item) => item.firstName === employeeName || item.lastName === employeeName);
+  return employee || {};
 };
 
 module.exports = getEmployeeByName;
