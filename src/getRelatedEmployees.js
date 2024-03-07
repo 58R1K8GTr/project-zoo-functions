100527c4d1e04253cc248c839c6c5550e2d2a27d
b166erobot <bernard0freitas@outlook.com>
2024-03-07 16:33:54 -0300

    Requisito 4 satisfeito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,21 @@ const data = require('../data/zoo_data');
 
 const isManager = (id) => {
   // seu código aqui
+  const isManager = data.employees
+    .some(({ managers }) => managers.includes(id));
+  return isManager;
 };
 
 const getRelatedEmployees = (managerId) => {
   // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const employeePeople = data.employees
+    .filter(({ managers }) => managers.includes(managerId));
+  const employeePeopleNames = employeePeople
+    .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  return employeePeopleNames;
 };
 
 module.exports = { isManager, getRelatedEmployees };
