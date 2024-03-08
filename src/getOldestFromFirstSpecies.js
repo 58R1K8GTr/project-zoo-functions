3b4f22444fb598560b77b9a1b7d95a02c138870b
no_name <no@email.com>
2024-03-08 00:04:16 -0300

    Requisito 10 satisfeito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,21 @@ const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
   // seu código aqui
+  const person = data.employees
+    .find(({ id: idPerson }) => id === idPerson);
+  const specieId = person.responsibleFor[0];
+  const olderAnimal = data.species
+    .find(({ id: id2 }) => specieId === id2)
+    .residents.reduce((acc, curr) => {
+      let returnValue;
+      if (acc.age < curr.age) {
+        returnValue = curr;
+      } else {
+        returnValue = acc;
+      }
+      return returnValue;
+    }, { age: 0 });
+  return Object.values(olderAnimal);
 };
 
 module.exports = getOldestFromFirstSpecies;
