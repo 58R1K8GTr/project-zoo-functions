9b7065ec62a17d8661bcbb8b6611b2c6e5265cdc
no_name <no@email.com>
2024-03-08 18:59:16 -0300

    Requisito 11 satisfeito

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,37 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
+function findEmployee(name, id) {
+  const person = data.employees
+    .find((employee) => employee.firstName === name
+      || employee.lastName === name || employee.id === id);
+  if (!person) throw new Error('Informações inválidas');
+  return person;
+}
+
+function getAReport(name, id) {
+  const person = findEmployee(name, id);
+  const species = data.species
+    .filter(({ id: idSpecie }) => person.responsibleFor.includes(idSpecie));
+  const { firstName, lastName } = person;
+  return {
+    id: person.id,
+    fullName: `${firstName} ${lastName}`,
+    species: species.map(({ name: nameSpecie }) => nameSpecie),
+    locations: species.map(({ location }) => location),
+  };
+}
+
+const getEmployeesCoverage = (personIdentifier) => {
   // seu código aqui
+  let result;
+  if (!personIdentifier) {
+    result = data.employees
+      .map(({ id: idEmployee }) => getAReport(undefined, idEmployee));
+  } else {
+    const { name = undefined, id = undefined } = personIdentifier;
+    result = getAReport(name, id);
+  }
+  return result;
 };
 
 module.exports = getEmployeesCoverage;
